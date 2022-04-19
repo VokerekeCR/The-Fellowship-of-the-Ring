5d284621da91abebbbe71e0ab25672c2d82ad65d
Amanda-Ruzza <amanda.ruzza@cloudreach.com>
2022-04-19 15:30:37 -0400

    Instructions on number_file

--- number_file.sh
+++ number_file.sh
@@ -1,4 +1,4 @@
 #This will be the user data file for the 1st instance
-#This script should execute only all the servers are online
+#This script should be executed only when all the servers are "turned on" + online
 if server IP = 192.168.10.230 ;
     then echo 1 >> NUMBER_FILE.txt
