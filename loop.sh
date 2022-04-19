95ac11759b588644285b54bd11cb3fb8f82c8c26
Delorrres <deloresmawuse@gmail.com>
2022-04-19 13:01:27 -0500

    Updated script -forgot to save

--- loop.sh
+++ loop.sh
@@ -3,14 +3,9 @@
 SERVER_LIST= /path/to/SERVER_LIST.txt
 NUMBER_FILE= /path/to/NUMBER_FILE.txt
 
-#Loop script
-for server in $SERVER”_LIST;  do
-
-#SSH COMMAND HERE
-ssh -v -i "my_pem.pem" ubuntu@<AmazonNode's_IP>
-
-#ping
-ping <EC2 EIP>
-    
-
-done  < $SERVER_LIST
+        #Loop script
+        for server in $SERVER_LIST;  do
+            #ping
+            ping <EC2 EIP>
+                
+        done  < $SERVER_LIST
